d240513c16a4ac222f05dcfca702b773ea907b32
ajprobel <ajprobel@gmail.com>
2024-05-09 21:41:20 -0400

    debug deployment issue - add dependencies, alter route logic

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,9 +3,10 @@ const { User } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
-    const userData = await userData.findAll();
+    const userData = await User.findAll();
     const users = userData.map((user) => user.get({ plain: true }));
     console.log(users);
+    res.status(200).json(users);
   } catch (err) {
     res.status(500).json(err);
   }
