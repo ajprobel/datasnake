bfa5e1b0e04b9172beec3cdbd618d2be0aba409e
MajorBriggs <158084320+danrcross@users.noreply.github.com>
2024-05-13 18:59:24 -0400

    added some routes. want to make a note that I added two lines to the server.js, to implement helper function

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../models");
+const { User, Score } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
@@ -12,4 +12,43 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/score", async (req, res) => {
+  try {
+    const scoreData = await Scores.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+    const scores = scoreData.map((score) => score.get({ plain: true }));
+    console.log(scores);
+    res.render("highscores", scores);
+    res.status(200).json(scores);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/makeaccount", async (req, res) => {
+  res.render("makeaccount");
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/userinfo");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/userinfo", (req, res) => {
+  res.render("userinfo");
+});
+
+router.get("/finduser", (req, res) => {
+  res.render("finduser");
+});
 module.exports = router;
