01d8b6c449fd030b589536aaab6de41453cd2f73
ajprobel <ajprobel@gmail.com>
2024-05-14 18:43:07 -0400

    update model relationships to one-to-many

--- seeds/scoreData.js
+++ seeds/scoreData.js
@@ -4,27 +4,37 @@ const scoredata = [
   {
     id: 1,
     user_id: 1,
-    high_score: 77,
+    score: 77,
   },
   {
     id: 2,
     user_id: 5,
-    high_score: 323,
+    score: 323,
   },
   {
     id: 3,
     user_id: 2,
-    high_score: 513,
+    score: 513,
   },
   {
     id: 4,
     user_id: 4,
-    high_score: 600,
+    score: 600,
   },
   {
     id: 5,
     user_id: 3,
-    high_score: 15,
+    score: 15,
+  },
+  {
+    id: 6,
+    user_id: 1,
+    score: 120,
+  },
+  {
+    id: 7,
+    user_id: 1,
+    score: 899,
   },
 
 ];
