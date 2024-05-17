8f292bf316bfe66369f2fe5127b7df367329677e
Husky Lover <ssolivares0@gmail.com>
2024-05-17 18:33:51 -0400

    added ID's to css, linked script to handlebars, and created anime.js file for anime technology

--- public/js/anime.js
+++ public/js/anime.js
@@ -1,49 +1,55 @@
-const text = document.querySelector("h2");
+const text = document.querySelector("#title");
 text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
-const snake = document.querySelector('#snake');
 
-// anime({
-//     targets: '.text',
-//     translateX: 250,
-//     rotate: '1turn',
-//     backgroundColor: '#FFF',
-//     duration: 800
-//   });
+anime
+  .timeline({
+    loop: false,
+  })
+  .add({
+    targets: "#title span",
+    translateY: [-600, 0],
+    scale: [6, 1],
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 800,
+    delay: anime.stagger(100),
+  })
+  .add({
+    targets: "#title span",
+    translateX: [0, -1000],
+    scale: [1, 1],
+    opacity: [1, 0],
+    easing: "easeOutExpo",
+    duration: 800,
+    delay: anime.stagger(100),
+  })
+  .add({
+    targets: "#title span",
+    translateX: [1000, 0],
+    scale: [1, 1],
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: anime.stagger(100),
+  });
 
-anime.timeline({
-  loop: false
-})
-.add({
-  targets: 'h2 span',
-  translateY: [-600,0],
-  scale: [6,1],
-  opacity: [0,1],
-  easing: 'easeOutExpo',
-  duration: 1500,
-  delay: anime.stagger(100)
-})
-.add({
-  targets: 'h2 span',
-  translateX: [0,-1000],
-  scale: [1,1],
-  opacity: [1,0],
-  easing: 'easeOutExpo',
-  duration: 1500,
-  delay: anime.stagger(100)
-})
-.add({
-  targets: 'h2 span',
-  translateX: [-1000,0],
-  scale: [1,1],
-  opacity: [0,1],
-  easing: 'easeOutExpo',
-  duration: 1000,
-  delay: anime.stagger(100)
-})
-.add({
-  targets: '#snake',
-  translateX: 200,
-  direction: 'alternate',
-  loop: true,
-  easing: 'steps(5)'
-})
+anime({
+  targets: "#snake",
+  rotate: {
+    value: 1440,
+    duration: 4000,
+    easing: "easeInOutSine",
+  },
+});
+
+anime({
+  targets: "#home-btn",
+  translateY: [120,0],
+  easing: "easeOutExpo"
+});
+
+anime({
+  targets: "#navbarNav",
+  translateY: [120,0],
+  easing: "easeOutExpo"
+});
